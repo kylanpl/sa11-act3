00d2b0cb69b54c225fcb6da722d015865dfda3cb
Kylan Plunk <kaplunk1@memphis.edu>
2024-04-24 03:34:27 -0500

    url extractor

--- url_extractor.rb
+++ url_extractor.rb
@@ -1,4 +1,8 @@
 def extract_urls(text)
+  pattern = /\w+:\/\/[w{3}\.]?\w+[\.\w+]+[^\.\s]+/
+  urls = text.scan(pattern)
+  puts urls.flatten
+
 end
 sample_text = "Visit our site at http://www.example.org for more
 information. Check out our search page at
