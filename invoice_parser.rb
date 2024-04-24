f7da460e6f913b914070ccc9542cd27ba62deee9
Kylan Plunk <kaplunk1@memphis.edu>
2024-04-24 02:54:36 -0500

    initial commit, add templates

---
+++ invoice_parser.rb
@@ -0,0 +1,8 @@
+def parse_invoices(invoice_data)
+end
+invoice_entries = <<-INVOICES
+2023-03-01 - INV001 - Acme Corp - $1000
+2023-03-02 - INV002 - Beta LLC - $2050
+2023-03-03 - INV003 - Gamma Inc - $3500
+INVOICES
+parse_invoices(invoice_entries)
