7bca41a0f73e09c25b7578c1aa5f30910f5f6b23
Kylan Plunk <kaplunk1@memphis.edu>
2024-04-24 03:52:06 -0500

    invoice parser

--- invoice_parser.rb
+++ invoice_parser.rb
@@ -1,7 +1,11 @@
 def parse_invoices(invoice_data)
-  pattern = /(\d{4}-\d{2}-\d{2})/
-  invoice = invoice_data.scan(pattern)
-  puts invoice.flatten
+  # It specifically says invoice NUMBERS are alphanumeric, so I'm allowing for letters in the invoice NUMBER, not just counting INV
+  pattern = /(\d{4}-\d{2}-\d{2}) - (INV\w+) - ([^\-]+) - (\$\d+)/
+  invoices = invoice_data.scan(pattern)
+  invoices.each do |invoice|
+    puts "Date: #{invoice[0]}\n Invoice Number #{invoice[1]}\n Company: #{invoice[2]}\n Amount: #{invoice[3]}"
+    puts "---"
+  end
 
 end
 invoice_entries = <<-INVOICES
